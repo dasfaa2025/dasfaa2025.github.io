56d9e3bd8dccae5ac3cd8f4bb138799af9f29033
z384647062 <384647062@qq.com>
2024-09-13 11:14:25 +0800

    chore: update avatars

--- src/components/MemberCard/index.tsx
+++ src/components/MemberCard/index.tsx
@@ -8,7 +8,7 @@ const MemberCard = ({
   return (
     <div>
       <img
-        className="m-0 mb-2 aspect-square w-full rounded-xl block transition-all hover:shadow-lg"
+        className="m-0 mb-2 aspect-square w-full rounded-2xl block transition-all duration-300 hover:shadow-lg max-sm:rounded-xl"
         src={cover}
       />
       <div className="text-center">{title}</div>
--- src/pages/organization/organizing-committee.tsx
+++ src/pages/organization/organizing-committee.tsx
@@ -1,16 +1,30 @@
 import MemberCard from "../../components/MemberCard";
 import Banner from "../../assets/singapore-3.jpg";
 import PageBanner from "../../components/PageBanner";
+import AvaterAkiyoNADAMOTO from "../../assets/avatars/Akiyo NADAMOTO.jpg";
+import AvaterBingxueZhang from "../../assets/avatars/Bingxue Zhang.jpg";
+import AvaterEePengLim from "../../assets/avatars/Ee-Peng Lim.jpg";
+import AvaterFeidaZhu from "../../assets/avatars/Feida Zhu.jpg";
+import AvaterKyuseokShim from "../../assets/avatars/Kyuseok Shim.jpg";
+import AvaterPhilipSYu from "../../assets/avatars/Philip S. Yu.jpg";
+import AvaterSouravBhowmick from "../../assets/avatars/Sourav Bhowmick.jpg";
+import AvaterWeiDing from "../../assets/avatars/Wei Ding.jpg";
+import AvaterXiaoliLI from "../../assets/avatars/Xiaoli LI.jpg";
+import AvaterYiYu from "../../assets/avatars/Yi Yu.jpg";
+import AvaterYuchenLI from "../../assets/avatars/Yuchen LI.jpg";
 
 const OrganizingCommittee = () => {
+  const memberlistClassName =
+    "grid grid-cols-3 gap-6 max-sm:grid-cols-2 max-sm:gap-6";
+
   return (
     <>
       <PageBanner title="Organizing Committee" image={Banner} />
       <article className="prose">
         <h2>General Co-Chairs</h2>
-        <div className="grid grid-cols-3 gap-8">
+        <div className={memberlistClassName}>
           <MemberCard
-            cover=""
+            cover={AvaterEePengLim}
             title={
               <>
                 <b>Ee-Peng Lim</b>, Singapore Management University, Singapore
@@ -18,7 +32,7 @@ const OrganizingCommittee = () => {
             }
           />
           <MemberCard
-            cover=""
+            cover={AvaterKyuseokShim}
             title={
               <>
                 <b>Kyuseok Shim</b>, Seoul National University, Korea
@@ -26,7 +40,7 @@ const OrganizingCommittee = () => {
             }
           />
           <MemberCard
-            cover=""
+            cover={AvaterWeiDing}
             title={
               <>
                 <b>Wei Ding</b>, UMass, USA
@@ -36,9 +50,9 @@ const OrganizingCommittee = () => {
         </div>
 
         <h2>Associate General Chairs</h2>
-        <div className="grid grid-cols-3 gap-8">
+        <div className={memberlistClassName}>
           <MemberCard
-            cover=""
+            cover={AvaterBingxueZhang}
             title={
               <>
                 <b>Bingxue Zhang</b>, University of Shanghai for Science and
@@ -49,9 +63,9 @@ const OrganizingCommittee = () => {
         </div>
 
         <h2>Program Committee Co-Chairs</h2>
-        <div className="grid grid-cols-3 gap-8">
+        <div className={memberlistClassName}>
           <MemberCard
-            cover=""
+            cover={AvaterPhilipSYu}
             title={
               <>
                 <b>Philip S. Yu</b>, University of Illinois at Chicago, USA
@@ -59,7 +73,7 @@ const OrganizingCommittee = () => {
             }
           />
           <MemberCard
-            cover=""
+            cover={AvaterFeidaZhu}
             title={
               <>
                 <b>Feida Zhu</b>, Singapore Management University, Singapore
@@ -67,7 +81,7 @@ const OrganizingCommittee = () => {
             }
           />
           <MemberCard
-            cover=""
+            cover={AvaterAkiyoNADAMOTO}
             title={
               <>
                 <b>Akiyo NADAMOTO</b>, Konan University, Japan
@@ -77,9 +91,9 @@ const OrganizingCommittee = () => {
         </div>
 
         <h2>Industry Program Co-Chairs</h2>
-        <div className="grid grid-cols-3 gap-8">
+        <div className={memberlistClassName}>
           <MemberCard
-            cover=""
+            cover={AvaterXiaoliLI}
             title={
               <>
                 <b>Xiaoli LI</b>, A*Star
@@ -91,9 +105,9 @@ const OrganizingCommittee = () => {
         <h2>Panel Chairs</h2>
 
         <h2>Workshop Chairs</h2>
-        <div className="grid grid-cols-3 gap-8">
+        <div className={memberlistClassName}>
           <MemberCard
-            cover=""
+            cover={AvaterSouravBhowmick}
             title={
               <>
                 <b>Sourav Bhowmick</b>, Nanyang Technological University,
@@ -104,9 +118,9 @@ const OrganizingCommittee = () => {
         </div>
 
         <h2>Tutorial Chairs</h2>
-        <div className="grid grid-cols-3 gap-8">
+        <div className={memberlistClassName}>
           <MemberCard
-            cover=""
+            cover={AvaterYiYu}
             title={
               <>
                 <b>Yi Yu</b>, Hiroshima University, Japan
@@ -116,9 +130,9 @@ const OrganizingCommittee = () => {
         </div>
 
         <h2>Demo Chairs</h2>
-        <div className="grid grid-cols-3 gap-8">
+        <div className={memberlistClassName}>
           <MemberCard
-            cover=""
+            cover={AvaterYuchenLI}
             title={
               <>
                 <b>Yuchen LI</b>, Singapore Management University, Singapore
